e7439d83a026663115f4049038ac2223ee97f222
lhjwork <jin.come.up.business@gmail.com>
2025-05-06 15:39:44 +0900

    URL 복사 버튼 만들기

--- src/components/testResult/ShareButtonGroup.jsx
+++ src/components/testResult/ShareButtonGroup.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, XIcon } from "react-share";
 import { base_url } from "../../App";
-// import
+import styles from "./shareButtonGroup.module.css";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 const ShareButtonGroup = ({ testParam, resultParam, renderTestInfo }) => {
   return (
     <div>
       <h3>친구에게 공휴하기</h3>
-      <div>
+      <div className={styles.shareButtonDiv}>
         <FacebookShareButton
           url={`${base_url}/${testParam}/result/${resultParam}`}
-          hashtag={`#${renderTestInfo?.info.mainTitle}`}
+          hashtag={`#${renderTestInfo?.info?.mainTitle}`}
         >
-          <FacebookIcon round={true} size={48} />
+          <FacebookIcon round={true} size={48} className={styles.socialMediaIcon} />
         </FacebookShareButton>
 
         <TwitterShareButton
           title={renderTestInfo?.info.mainTitle}
           url={`${base_url}/${testParam}/result/${resultParam}`}
-          hashtags={[renderTestInfo?.info.mainTitle]}
+          hashtags={[renderTestInfo?.info?.mainTitle]}
         >
-          <XIcon round={true} size={48} />
+          <XIcon round={true} size={48} className={styles.socialMediaIcon} />
         </TwitterShareButton>
+
+        <CopyToClipboard
+          text={`${base_url}/${testParam}/result/${resultParam}`}
+          onCopy={() => alert("URL이 복사되었습니다.")}
+        >
+          <button className={styles.urlShareButton}>URL</button>
+        </CopyToClipboard>
       </div>
     </div>
   );
